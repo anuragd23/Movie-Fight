ce63211502ef4864d6e00fa8a84b72feec374703
Anurag Das <anurag.das23.ad@gmail.com>
2021-03-21 19:45:49 -0400

    fetching Avengers movie data by ID

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '819b0d6b',
-            s: 'avengers'
+            i: 'tt0848228'
         }
     });
 
