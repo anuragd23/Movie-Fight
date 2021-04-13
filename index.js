0045071a4791759860b9573005df58fa1987250e
Anurag Das <anurag.das23.ad@gmail.com>
2021-04-12 21:08:31 -0400

    Defining create auto complete

--- index.js
+++ index.js
@@ -13,59 +13,8 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 }
 
-const root = document.querySelector('.autocomplete');
+createAutoComplete({
 
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input"/>
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-
-    if(!movies.length) {
-        dropdown.classList.remove('is-active');
-        return;
-    }
-
-    resultWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    
-    for(let movie of movies) {
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
-            <img src="${imgSrc}" />
-            ${movie.Title}
-        `;
-
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-    
-        resultWrapper.appendChild(option);
-    }
-};
-
-input.addEventListener('input', debounce(onInput));
-
-document.addEventListener('click', event => {
-    if(!root.contains(event.target)) {
-        dropdown.classList.remove('is-active');
-    }
 });
 
 const onMovieSelect = async movie => {
@@ -115,6 +64,6 @@ const movieTemplate = (movieDetail) => {
         <p class="title">${movieDetail.imdbVotes}</p>
         <p class="subtitle">IMDB Votes</p>
     </article>
-     `
+   `
 };
 
